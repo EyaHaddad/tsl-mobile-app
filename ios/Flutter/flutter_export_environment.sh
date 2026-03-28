db52286ebe0198e38d8f32d1fd97aee66eaac295
MalekToumi-815 <191920560+MalekToumi-815@users.noreply.github.com>
2026-03-28 13:57:50 +0100

    Regenerate Android platform (fix v1 embedding)

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\malek\dev\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\malek\tsl-mobile-app"
+export "FLUTTER_APPLICATION_PATH=C:\Users\malek\OneDrive\Desktop\projects\tsl-mobile-app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
